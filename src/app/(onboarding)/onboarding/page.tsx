026e2a863f30934739ca1a1208a98fbbbfdb4edf
Luisa_Cadena <luicadenav@gmail.com>
2025-06-18 00:20:26 -0500

    feat(signin): form add zod and reacthookform

--- src/app/(onboarding)/onboarding/page.tsx
+++ src/app/(onboarding)/onboarding/page.tsx
@@ -4,18 +4,20 @@ import Link from "next/link";
 
 function OnboardingPage() {
   return (
-    <div className="max-w-md h-full mx-auto bg-white flex flex-col items-center justify-center gap-5 p-4">
+    <div className="max-w-[430px] min-h-screen  overflow-y-auto mx-auto bg-white flex flex-col items-center justify-center gap-5 p-4">
       <OnboardingImg
+        width={340}
+        height={326}
         role="img"
         aria-label="onboarding img"
-        className="mb-7 w-full max-w-[326px] h-auto"
+        className="mb-7 w-full flex-shrink-0"
       />
       <LogoPaypot
         width={81}
         height={81}
         role="img"
         aria-label="app logo"
-        className=""
+        className="flex-shrink-0"
       />
       <h1 className="font-sans font-medium text-2xl ">PayPot</h1>
       <p className="font-sans font-medium text-md text-center text-secondary-2 ">
@@ -24,7 +26,7 @@ function OnboardingPage() {
 
       <Link
         href="/signin"
-        className="bg-primary-1 text-white text-center w-3/5 py-3 rounded-xl font-sans font-medium text-md cursor-pointer hover:bg-primary-2 transition-colors duration-200 ease-in-out"
+        className="bg-primary-1 text-white text-center w-3/5 py-3 mb-20 mt-8 rounded-xl font-sans font-medium text-md cursor-pointer hover:bg-primary-2 transition-colors duration-200 ease-in-out"
       >
         Get Started
       </Link>
--- src/app/(onboarding)/signin/page.tsx
+++ src/app/(onboarding)/signin/page.tsx
@@ -1,27 +1,115 @@
 "use client";
 import TopBar from "@/components/common/TopBar";
 import LockIllustration from "@/assets/images/lock.svg";
-
 import React, { useState } from "react";
-import CustomInput from "@/components/common/CustomInput";
+import { SubmitHandler, useForm } from "react-hook-form";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { Eye, EyeOff } from "lucide-react";
+import Link from "next/link";
+
+const schema = z.object({
+  email: z.string().email(),
+  password: z.string().min(8),
+});
+
+type FormFields = z.infer<typeof schema>;
 
 function SignIn() {
-  const [value, setvalue] = useState("");
+  const [showPassword, setShowPassword] = useState(false);
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors, isSubmitting },
+  } = useForm<FormFields>({
+    resolver: zodResolver(schema), // way to connect zod
+  });
+
+  const onSubmit: SubmitHandler<FormFields> = (data) => {
+    console.log(data);
+  };
+
   return (
-    <div className="max-w-md h-full mx-auto bg-white flex flex-col  gap-5 p-4 ">
+    <div className="max-w-md h-dvh  overflow-y-auto mx-auto bg-white flex flex-col  gap-5 p-4 ">
       <TopBar name="Sign In" />
-      <LockIllustration className="mx-auto my-16" />
-      <form>
-        <CustomInput
-          type="email"
-          label="Email"
-          value={value}
-          onChange={(e) => {
-            setvalue(e.target.value);
-            console.log(e.target);
-          }}
-          name="email"
-        />
+      <LockIllustration className="mx-auto my-12  flex-shrink-0" />
+      <form
+        onSubmit={handleSubmit(onSubmit)}
+        className="w-full flex flex-col gap-8 "
+      >
+        <div className="w-full flex flex-col">
+          <label
+            htmlFor="email"
+            className="text-sm mb-2 text-secondary-3 capitalize peer-focus:text-primary-1 peer-focus:font-semibold"
+          >
+            Email
+          </label>
+          <input
+            {...register("email")}
+            type="text"
+            id="email"
+            className="w-full  border-2 border-secondary-5 rounded-lg p-3 text-secondary-2  focus:outline-none focus:ring-2 focus:ring-primary-1  focus:bg-primary-4 peer "
+            placeholder="Email"
+          />
+
+          {errors.email && (
+            <span className="text-semantic-1 text-xs mt-0.5">
+              {errors.email.message}{" "}
+            </span>
+          )}
+        </div>
+        <div className="w-full flex flex-col relative">
+          <label
+            htmlFor="password"
+            className="text-sm mb-2 text-secondary-3 capitalize peer-focus:text-primary-1 peer-focus:font-semibold"
+          >
+            Password
+          </label>
+          <input
+            {...register("password")}
+            type={showPassword ? "text" : "password"}
+            id="password"
+            className="w-full  border-2 border-secondary-5 rounded-lg p-3 text-secondary-2  focus:outline-none focus:ring-2 focus:ring-primary-1  focus:bg-primary-4 peer "
+            placeholder="Password"
+          />
+          <button
+            type="button"
+            onClick={() => setShowPassword((prev) => !prev)}
+            className="absolute right-4 top-11"
+          >
+            {showPassword ? (
+              <EyeOff size={20} color="#989898" />
+            ) : (
+              <Eye size={20} color="#989898" />
+            )}
+          </button>
+          {errors.password && (
+            <span className="text-semantic-1 text-xs mt-0.5">
+              {errors.password.message}{" "}
+            </span>
+          )}
+          <Link
+            href="/signup"
+            className="text-sm text-primary-2 hover:underline text-right mt-1"
+          >
+            Forgot password ?
+          </Link>
+        </div>
+        <div>
+          <button
+            type="submit"
+            className="bg-primary-1 text-white text-center w-full py-3 mt-8 rounded-xl font-sans font-medium text-md cursor-pointer hover:bg-primary-2 transition-colors duration-200 ease-in-out"
+          >
+            Sign In
+          </button>
+          <p className="text-sm text-secondary-1 text-center mt-1 mb-20 ">
+            Don&apos;t have an account?{" "}
+            <Link href="/signup" className="text-primary-2 hover:underline">
+              Sign Up
+            </Link>
+          </p>
+        </div>
       </form>
     </div>
   );
